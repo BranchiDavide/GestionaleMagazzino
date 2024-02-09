b99d061237a2fbf30b5cbe56feb457c7c3113f72
CappellariGioele <gioele.cappellari@samtrevano.ch>
2024-02-09 15:09:30 +0100

    Interfacce in ejs e iniziato parte di collegamento view con controller

--- 5_Applicativo/GestionaleMagazzino/app.js
+++ 5_Applicativo/GestionaleMagazzino/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const env = require('dotenv').config();
 
 const app = express();
@@ -7,14 +8,14 @@ const PORT = process.env.PORT;
 // configurazione server
 app.set("view engine", "ejs");
 app.set("views", "views");
-app.use("/css", express.static(__dirname + "/node_modules/bootstrap/dist/css"));
-app.use("/js", express.static(__dirname + "/node_modules/bootstrap/dist"));
+app.use("/bootstrap", express.static(__dirname + "/node_modules/bootstrap/dist"));
+app.use("/font", express.static(__dirname + "/public/font"))
+app.use("/css", express.static(__dirname + "/public/css"));
+app.use("/img", express.static(__dirname + "/public/img"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use("/", require("./routes/index"));
 
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
-});
-
-app.get('/', (req, res) => {
-    res.send("The auto deploy works!");
 });
