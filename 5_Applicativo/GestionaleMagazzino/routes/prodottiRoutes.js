d2f2fcfb9c3b3d86bd1b86d0ca78f6575be51e99
CappellariGioele <gioele.cappellari@samtrevano.ch>
2024-03-02 13:09:18 +0100

    Installato sanitize-html parte giusta

--- 5_Applicativo/GestionaleMagazzino/routes/prodottiRoutes.js
+++ 5_Applicativo/GestionaleMagazzino/routes/prodottiRoutes.js
@@ -6,5 +6,8 @@ const prodottiController = require("./../controllers/prodottiController");
 const authMiddleware = require('../middlewares/authMiddleware');
 
 router.get("/prodotti", authMiddleware.isAuthenticated, prodottiController.showAll)
+router.get("/dettagli_prodotto", authMiddleware.isAuthenticated, (req, res) => {
+    res.status(200).render("prodotto/dettaglio.ejs");
+})
 
 module.exports = router;
