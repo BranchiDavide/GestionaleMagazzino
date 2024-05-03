08a0bb966587a87adca5f94f61384c62a32378f3
Davide Branchi <davide.branchi@samtrevano.ch>
2024-05-03 13:58:22 +0200

    Aggiunta dei manuali all'applicativo

--- 5_Applicativo/GestionaleMagazzino/routes/manualeRoutes.js
+++ 5_Applicativo/GestionaleMagazzino/routes/manualeRoutes.js
@@ -5,8 +5,8 @@ const authMiddleware = require('../middlewares/authMiddleware');
 const router = express.Router();
 
 router.get("/manuale", authMiddleware.isAuthenticated, manualeController.loadViewManuale)
-router.get("/manuale/utente", authMiddleware.isAuthenticated, manualeController.showManualeUtente("doc2.pdf"))
-router.get("/manuale/gestore", authMiddleware.isAuthenticated, authMiddleware.isGestore, manualeController.showManualeUtente("doc2.pdf"))
-router.get("/manuale/amministratore", authMiddleware.isAuthenticated, authMiddleware.isAmministratore, manualeController.showManualeUtente("doc2.pdf"))
+router.get("/manuale/utente", authMiddleware.isAuthenticated, manualeController.showManualeUtente("Manuale_Utente.pdf"))
+router.get("/manuale/gestore", authMiddleware.isAuthenticated, authMiddleware.isGestore, manualeController.showManualeUtente("Manuale_Gestore.pdf"))
+router.get("/manuale/amministratore", authMiddleware.isAuthenticated, authMiddleware.isAmministratore, manualeController.showManualeUtente("Manuale_Amministratore.pdf"))
 
 module.exports = router;
